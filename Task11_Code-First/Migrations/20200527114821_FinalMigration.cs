defb7e1a7a4df2389bfe007f732355b7f066d311
Kamil <sikorakamil26>
2020-05-27 14:04:44 +0200

    finished task 11

---
+++ Task11_Code-First/Migrations/20200527114821_FinalMigration.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task3.Migrations
+{
+    public partial class FinalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 48, 21, 500, DateTimeKind.Local).AddTicks(7340));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 48, 21, 503, DateTimeKind.Local).AddTicks(1640));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 48, 21, 503, DateTimeKind.Local).AddTicks(1720));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 1,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 4, 23, 544, DateTimeKind.Local).AddTicks(1950));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 2,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 4, 23, 545, DateTimeKind.Local).AddTicks(8610));
+
+            migrationBuilder.UpdateData(
+                table: "Prescription",
+                keyColumn: "IdPrescription",
+                keyValue: 3,
+                column: "Date",
+                value: new DateTime(2020, 5, 27, 13, 4, 23, 545, DateTimeKind.Local).AddTicks(8690));
+        }
+    }
+}
