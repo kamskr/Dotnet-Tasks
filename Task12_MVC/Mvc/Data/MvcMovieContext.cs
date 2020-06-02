142d43c1b6c08f7067d49c87fbafa28e9289f847
Kamil <sikorakamil26>
2020-06-02 17:30:23 +0200

    initial commit

---
+++ Task12_MVC/Mvc/Data/MvcMovieContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mvc.Models;
+
+namespace Mvc.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
